9a71078961908f9ab599ee523c7474aff2e6c195
Alejandro Arias <andrauca@espol.edu.ec>
2025-11-12 19:08:37 +0000

    guia 1213

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import { Grid } from '@mui/material';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     
-      <Grid>
-
+      <Grid container spacing={5} justifyContent="center" alignItems="center">
+        <div>
+            <h1>Bienvenido al Dashboard</h1>
+        </div>
          {/* Encabezado */}
-         <Grid>Elemento: Encabezado</Grid>
+         <Grid size={{xs:12,md:12}}>
+        <HeaderUI/>
+         </Grid>
 
          {/* Alertas */}
-         <Grid>Elemento: Alertas</Grid>
+         <Grid size={{xs:12,md:12}}  container justifyContent="right" alignItems="center">
+          <AlertUI description="No se preveen lluvias"/>
+
+         </Grid>
 
          {/* Selector */}
-         <Grid>Elemento: Selector</Grid>
+         <Grid size = {{xs:12,md:3}}>Elemento: Selector</Grid>
 
          {/* Indicadores */}
-         <Grid>Elemento: Indicadores</Grid>
+         <Grid size = {{xs:12,md:9}}>Elemento: Indicadores</Grid>
 
          {/* Gráfico */}
-         <Grid>Elemento: Gráfico</Grid>
+         <Grid size={{xs:12,md:6}} sx={{ display: {xs:"none",md:"block" }}}>Elemento: Gráfico</Grid>
 
          {/* Tabla */}
-         <Grid>Elemento: Tabla</Grid>
+         <Grid size={{xs:12,md:6}} sx={{ display: {xs:"none",md:"block" }}}>Elemento: Tabla</Grid>
 
          {/* Información adicional */}
-         <Grid>Elemento: Información adicional</Grid>
+         <Grid size={{xs:12,md:12}}>Elemento: Información adicional</Grid>
 
       </Grid>
   )
